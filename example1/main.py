2ffca5299c758cb874a8a8f6571f20df0b09b040
Saad Guessous <guessous_saad@hotmail.com>
2022-07-02 22:28:45 +0300

    Add module example 1

---
+++ example1/main.py
@@ -0,0 +1,13 @@
+import module1
+import sys
+
+
+print('=============================================================')
+
+print(f'Running main.py - module name: {__name__}')
+
+
+print(sys.modules)
+
+
+print('=============================================================')
